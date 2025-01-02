795ebe984dc54c8c4b141ee894ea320eea6c2395
krishankantkushwaha <krishankantkushwaha719@gmail.com>
2025-01-02 14:43:33 +0530

    update

--- src/main/java/com/mainSp/mainSp.java
+++ src/main/java/com/mainSp/mainSp.java
@@ -2,8 +2,6 @@ package com.mainSp;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.model.Child;
 import com.model.Employee;
 
 public class mainSp {
@@ -12,18 +10,7 @@ public class mainSp {
 		ApplicationContext ioc = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 		Employee e = (Employee)ioc.getBean("b1");
 		System.out.println(e);
-//		Employee e2 = (Employee)ioc.getBean("b2");
-//		e2.getAddress().setCity("noida");
-//		e2.getAddress().sethNO(310);
-//		e2.getAddress().setState("UP");
-//		System.out.println(e2);
-
-	                	// Parent-Child//
-
-//		ApplicationContext ioc = new ClassPathXmlApplicationContext("parent_child.xml");
-//	
-//		Child emp = (Child) ioc.getBean("e2");
-//		System.out.println(emp);
+	
 		
 	}
 
